61ca5a9dd3360d8caefb90c0ca93ece458e7d649
Serhii Holub <serhii.holub@valtech.com>
2024-09-03 11:46:56 +0300

    Section 2: Creating and Connecting Vue App Instances

---
+++ section-2/basics-01-starting-code/app.js
@@ -0,0 +1,10 @@
+console.log(Vue)
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finish the course and learn Vue!'
+        }
+    }
+});
+
+app.mount('#user-goal');
