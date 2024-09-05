32436c2d99211ddec59c6cfe392ed8f3bbb01f7f
Serhii Holub <serhii.holub@valtech.com>
2024-09-05 10:30:18 +0300

    Section 2:  Working with Data inside of a Vue App

--- section-2/basics-01-starting-code/app.js
+++ section-2/basics-01-starting-code/app.js
@@ -2,7 +2,8 @@ console.log(Vue)
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master vue and and build amazing apps!',
             vueLink: 'https://vuejs.org/'
         }
     },
@@ -10,9 +11,9 @@ const app = Vue.createApp({
         outputGoal() {
             const randomNumber = Math.random();
             if(randomNumber < 0.5) {
-                return 'Learn Vue!';
+                return this.courseGoalA;
             } else {
-                return 'Master Vue!';
+                return this.courseGoalB;
             }
         }
     }
